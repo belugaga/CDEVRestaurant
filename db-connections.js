cc018a0911ddc8e6dd7c4a68957cca40640afbe9
belugaga <haorui321@gmail.com>
2022-11-21 12:12:49 +0800

    Update db-connections.js

--- db-connections.js
+++ db-connections.js
@@ -1,15 +1,15 @@
 var mysql = require('mysql2');
 var connection = mysql.createConnection({
-    host: 'dbrds1.cjb5vrcyxql7.ap-southeast-1.rds.amazonaws.com',
+    host: '127.0.0.1',
     port: '3306',
-    user: 'admin',
-    password: 'tG078568',
+    user: 'root',
+    password: 'cdev',
     database: 'projectdb'
 });
 
 connection.connect(err => {
     if(err) throw err;
-    console.log('Connected To AWS DB');
+    console.log('Connected To DB');
 });
 
 module.exports = connection;
